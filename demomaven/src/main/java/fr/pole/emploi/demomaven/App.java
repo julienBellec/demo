c404e7a64c8c48e2690bb8fa2b27ac4c4f7cc512
IJBE3080 <IJBE3080@P37069.dgasi.pole-emploi.intra>
2018-11-12 20:18:49 +0100

    Init

---
+++ demomaven/src/main/java/fr/pole/emploi/demomaven/App.java
@@ -0,0 +1,13 @@
+package fr.pole.emploi.demomaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
