761a7515f395c1fa60950f5a0e5193c6f9c35c6a
Julien Bellec <julien.bellec@pole-emploi.fr>
2019-08-22 15:38:49 +0200

    Date

---
+++ cleancode/src/main/java/fr/jbellec/demo/date/HumanTime.java
@@ -0,0 +1,29 @@
+package fr.jbellec.demo.date;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+public class HumanTime {
+	public static void main(String[] args) {
+		LocalDateTime currentTime = LocalDateTime.now();
+		System.out.println("CurrentTime : " + currentTime.toString());
+		LocalDate currentDate = currentTime.toLocalDate();
+		System.out.println("CurrentDate : " + currentDate.toString());
+		System.out.println("Mois : " + currentDate.getMonth() + " - Day : " + currentDate.getDayOfWeek());
+
+		LocalDateTime christmas = LocalDateTime.now().withMonth(12).withDayOfMonth(25);
+
+		System.out.println("christmas  : " + christmas.format(DateTimeFormatter.ISO_LOCAL_DATE));
+
+		ZonedDateTime zonedDateTime = ZonedDateTime.of(christmas, ZoneId.of("Europe/Helsinki"));
+		System.out.println(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG).format(zonedDateTime));
+
+		String europeanDatePattern = "dd MMMM yyyy";
+		DateTimeFormatter europeanDateFormatter = DateTimeFormatter.ofPattern(europeanDatePattern);
+		System.out.println(europeanDateFormatter.format(christmas));
+	}
+}
