d77a84e8bf931e9516833e00274fe6c4b0e34d0a
Julien Bellec <julien.bellec@pole-emploi.fr>
2022-09-12 05:59:56 +0200

    Update JUNIT

---
+++ kata/src/main/java/fr/jbellec/kata/leapyear/LeapYear.java
@@ -0,0 +1,7 @@
+package fr.jbellec.kata.leapyear;
+
+public class LeapYear {
+    public static boolean isLeapYear(int year) {
+        return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+    }
+}
---
+++ kata/src/test/java/fr/jbellec/kata/leapyear/LeapYearTest.java
@@ -0,0 +1,16 @@
+package fr.jbellec.kata.leapyear;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class LeapYearTest {
+
+    @ParameterizedTest
+    @CsvSource({"400,true", "100,false", "200,false", "800,true", "2000,true", "1700, false", "1800, false", "1900, false", "2008, true", "2012, true", "2016, true"})
+    void testIsLeapYear(int year, String isLeapYear){
+        assertEquals(Boolean.valueOf(isLeapYear), LeapYear.isLeapYear(year));
+    }
+
+}
