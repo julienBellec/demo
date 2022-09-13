278e5f4a143c101e7ececd89cb4f29981a28463d
Julien Bellec <julien.bellec@pole-emploi.fr>
2022-09-13 04:44:29 +0200

    Update LeapYear

--- kata/src/test/java/fr/jbellec/kata/leapyear/LeapYearTest.java
+++ kata/src/test/java/fr/jbellec/kata/leapyear/LeapYearTest.java
@@ -9,7 +9,7 @@ class LeapYearTest {
 
     @ParameterizedTest
     @CsvSource({"400,true", "100,false", "200,false", "800,true", "2000,true", "1700, false", "1800, false", "1900, false", "2008, true", "2012, true", "2016, true"})
-    void testIsLeapYear(int year, String isLeapYear){
+    void testIsLeapYear(int year, String isLeapYear) {
         assertEquals(Boolean.valueOf(isLeapYear), LeapYear.isLeapYear(year));
     }
 
